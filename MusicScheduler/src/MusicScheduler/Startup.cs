0ad372067f5c6aeec91f5aad07dfc4092742cb27
Norman Ackermann <norman_ackermann@web.de>
2015-12-15 19:50:26 +0100

    moved MusicScheduler v1 to GitHub

---
+++ MusicScheduler/src/MusicScheduler/Startup.cs
@@ -0,0 +1,79 @@
+﻿using MediaToolkit;
+using MediaToolkit.Model;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Diagnostics;
+using Microsoft.AspNet.Diagnostics.Entity;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Framework.Configuration;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.Logging;
+using Microsoft.Framework.Runtime;
+using MusicScheduler.Objects;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using YoutubeExtractor;
+
+namespace MusicScheduler
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env, IApplicationEnvironment appEnv)
+        {
+            try
+            {
+                Directory.Delete(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/Downloaded"), true);
+            }
+            catch (Exception)
+            {
+                // directory wurde nicht gefunden, weil erster start, ist wurst, geht auch bestimmt ohne try catch, aber zu faul :)
+            }
+
+            ServiceLocator.Initalize();
+
+            var builder = new ConfigurationBuilder(appEnv.ApplicationBasePath)
+                .AddJsonFile("config.json")
+                .AddJsonFile($"config.{env.EnvironmentName}.json", optional: true);
+
+            if (env.IsDevelopment())
+            {
+                builder.AddUserSecrets();
+            }
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // Configure is called after ConfigureServices is called.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.MinimumLevel = LogLevel.Information;
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseErrorPage(ErrorPageOptions.ShowAll);
+                app.UseDatabaseErrorPage(DatabaseErrorPageOptions.ShowAll);
+            }
+            else
+            {
+                app.UseErrorHandler("/Home/Error");
+            }
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
