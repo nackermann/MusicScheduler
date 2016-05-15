20ee58dad11ec72ae408261b32ef23f8fb1dbfb3
Norman Ackermann <norman_ackermann@web.de>
2016-05-15 16:31:43 +0200

    added the new vNext asp project

---
+++ MusicScheduler/src/MusicScheduler/ViewModels/Manage/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicScheduler.ViewModels.Manage
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
